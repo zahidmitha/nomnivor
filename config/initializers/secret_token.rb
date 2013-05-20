8a4f84f6ec073d1bafa7444068a3cab0bda68b7b
samuel strong <s.j.p.strong@gmail.com>
2013-05-20 10:22:06 +0100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomnivor::Application.config.secret_token = '9dc48f9686ff2fc1d78ab6a9d21b0aada9bb1adc094d54054e4013cf9547b0faf9649ff9d107eff0cb2e39823c55a2fbb3f686720845af4046d4e75a7feb53be'
