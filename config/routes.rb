5c6304613600bb3bf3cefb4db57c63decd88483e
newton101 <miguil@hotmail.co.uk>
2013-05-20 14:20:45 +0100

    get example map

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Nomnivor::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+   resource :map, :controller => :map
+   # root :to => 'map#new'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
