e046991361b5c90222523d4d9a96c2b61736d838
Ruben Kostucki <rubenkostucki@gmail.com>
2013-05-21 13:31:06 +0100

    flash notification of restaurant additions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,9 +4,12 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.create(params[:restaurant])
 
     if @restaurant.valid?
+      flash[:notice] = "Restaurant has been added"
       redirect_to root_url
     else
       render :json => { :message => "The information you entered about the restaurant was not correct. Please try again!" }
     end
   end
+
+
 end
