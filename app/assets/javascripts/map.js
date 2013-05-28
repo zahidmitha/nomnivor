a2c585cf5f05a34ec0dba54e8073d61b885242b3
newton101 <miguil@hotmail.co.uk>
2013-05-28 20:23:46 +0100

    adding restaurants but not removing irrelevant ones

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,67 +1,68 @@
 $(document).ready(function() {
 
 
-
   var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([51.5, -0.08], 16);
 
- //  $.ajax({
-	// 	type: "GET",
-	// 	url: "/restaurants/",
-	// 	data: $(this).serialize(),
-	// 	success: function(data){
-	// 		$.each(data, function(index, item){
- //    			addMarker(item);
-	// 		});
-	// 	}
-	// });
+  // $.ajax({
+  //   type: "GET",
+  //   url: "/restaurants/",
+  //   data: $(this).serialize(),
+  //   success: function(data){
+  //     $.each(data, function(index, item){
+  //       addMarker(item);
+  //     });
+  //     ;
+  //   }
+
+  // });
 
   $(".diet-filter").submit(function(e){
+
       e.preventDefault();
       $.ajax({
         type: "GET",
         url: "/restaurants/",
         data: $(this).serialize(),
         success: function(data){
-          $.each(data, function(index, item) {
-                addMarker(item);
+            var jsonarray = []
+            var markerLayer = L.mapbox.markerLayer(jsonarray).addTo(map);
+            $.each(data, function(index, restaurant) {
+
+            // a simple GeoJSON featureset with a single point
+            // with no properties
+            var geo = markerLayer.setGeoJSON({
+                type: "FeatureCollection",
+                features: [{type: "Feature",
+                    geometry: {
+                    type: "Point",
+                    coordinates: [restaurant.latitude, restaurant.longitude]
+                     },
+                properties: { "title": restaurant.name, "description": restaurant.description}}]
             });
+             jsonarray.push(geo);
+            });
+
           }
-        });
+      });
+
   });
 
 
-  function addMarker(item) {
-		var geoJson = [{
-	    type: 'Feature',
-	    "geometry": { "type": "Point", "coordinates": [item.longitude, item.latitude]},
-	    "properties": {
-	        "url": "/",
-	        "name": item.name,
-	        "description": item.description
-	    }
+  //   function createGeo(item) {
+  // 		var geoJson = {
+  // 	    "type": "Feature",
+  // 	    "geometry": { "type": "Point", "coordinates": [item.latitude, item.longitude]},
+  // 	    "properties": {
+  // 	        "title": item.name,
+  // 	        "description": item.description
+  // 	    }
+  // 	 };
+  // //    map.markerLayer.setGeoJSON(geoJson);
+  // //    // return geoJson;
+  // // //popop code goes here
 
-	}];
+  // }
 
-	map.markerLayer.setGeoJSON(geoJson);
-	map.markerLayer.on('ready', function(e) {
-    this.eachLayer(function(marker) {
-        var feature = marker.feature;
-        // Create custom popup content
-        var popupContent =  '<a target="_blank" class="popup" href="' + feature.properties.url + '">' +
-             '<div>' + '</div>' + '</a>' + '<p>' + '</p>' + '<p>' + '<strong>' + 'Lat/Long:' + '</strong>' + " " + feature.geometry.coordinates + '</p>';
-
-        // http://leafletjs.com/reference.html#popup
-        marker.bindPopup(popupContent,{
-            closeButton: true,
-            minWidth: 320
-        });
-
-    });
-
-});
-
-
-	}
 
 
 
@@ -99,9 +100,25 @@ $('.multiselect').multiselect({
             selected += $(this).text() + ', ';
                 });
 
-          return selected.substr(0, selected.length -2) + ' <b class="caret"></b>';
+          return selected.substr(0, selected.length -2) + '<b class="caret"></b>';
         }
       },
   });
 
 });
+  // map.markerLayer.on('ready', function(e) {
+ //    this.eachLayer(function(marker) {
+ //        var feature = marker.feature;
+ //        // Create custom popup content
+ //        var popupContent =  '<a target="_blank" class="popup" href="' + feature.properties.url + '">' +
+ //             '<div>' + '</div>' + '</a>' + '<p>' +feature.properties.title + '</p>' + feature.properties.description +'<p>' + '<strong>' + 'Lat/Long:' + '</strong>' + " " + feature.geometry.coordinates + '</p>';
+
+ //        // http://leafletjs.com/reference.html#popup
+ //        marker.bindPopup(popupContent,{
+ //            closeButton: true,
+ //            minWidth: 320
+ //        });
+
+ //    });
+
+ //  });
